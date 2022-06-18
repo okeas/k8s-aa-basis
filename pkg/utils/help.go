56e97dd85b3b75f44a93983e5a11df377fa5efd6
zhengxin <zx358317301@qq.com>
2022-06-18 18:43:47 +0800

    修改

--- pkg/utils/help.go
+++ pkg/utils/help.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"k8s-aa-basis/pkg/apis/myingress/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -10,32 +9,9 @@ var (
 	Table_ListColumns = []string{"name", "namespace", "path", "host"}
 )
 
-func NotFoundStatus(msg string) *metav1.Status {
-	s := &metav1.Status{
-		Code:    404,
-		Message: msg,
-		Reason:  metav1.StatusReasonNotFound,
-		Status:  "Failure",
-	}
-	s.Kind = "Status"
-	s.APIVersion = "v1"
-	return s
-}
-
-func ErrorStatus(code int32, msg string, reason metav1.StatusReason) *metav1.Status {
-	s := &metav1.Status{
-		Code:    code,
-		Message: msg,
-		Reason:  reason,
-		Status:  "Failure",
-	}
-	s.Kind = "Status"
-	s.APIVersion = "v1"
-	return s
-}
-
 // 把列表 或单资源 变成表格化
 func ConvertToTable(obj interface{}) *metav1.Table {
+
 	t := &metav1.Table{}
 	t.Kind = "Table"
 	t.APIVersion = "meta.k8s.io/v1"
@@ -43,19 +19,57 @@ func ConvertToTable(obj interface{}) *metav1.Table {
 		//设置表头
 		th := make([]metav1.TableColumnDefinition, len(Table_ListColumns))
 		for i, h := range Table_ListColumns {
-			th[i] = metav1.TableColumnDefinition{Name: h, Type: "string"}
+			format := "name"
+
+			th[i] = metav1.TableColumnDefinition{Name: h, Type: "string", Format: format}
 		}
 		t.ColumnDefinitions = th //设置表头
 		//设置 行  数据
 		rows := make([]metav1.TableRow, len(v.Items))
 		for i, item := range v.Items {
-			fmt.Println(item)
 			rows[i] = metav1.TableRow{
-				Cells: []interface{}{item.Name, item.Namespace, item.Spec.Path, item.Spec.Host},
+				Cells: []interface{}{item.Name, item.Namespace,
+					item.Spec.Path, item.Spec.Host},
 			}
 		}
 		t.Rows = rows
 	}
 	return t
 
+}
+
+func SuccessStatus(message string) *metav1.Status {
+	status := &metav1.Status{
+		Code:    200,
+		Message: message,
+		Status:  "Success",
+	}
+	status.Kind = "Status"
+	status.APIVersion = "v1"
+	return status
+}
+
+func ErrorStatus(code int32, message string, reason metav1.StatusReason) *metav1.Status {
+	status := &metav1.Status{
+		Code:    code,
+		Message: message,
+		Reason:  reason,
+		Status:  "Failure",
+	}
+	status.Kind = "Status"
+	status.APIVersion = "v1"
+	return status
+}
+
+//失败状态
+func NotFoundStatus(message string) *metav1.Status {
+	status := &metav1.Status{
+		Code:    404,
+		Message: message,
+		Reason:  metav1.StatusReasonNotFound,
+		Status:  "Failure",
+	}
+	status.Kind = "Status"
+	status.APIVersion = "v1"
+	return status
 }
